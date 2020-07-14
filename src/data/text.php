bf8ae6df9dd1168bf24640545dd0b6954199f6de
unapersona <dev@unapersona.com>
2020-07-14 17:18:17 +0200

    Engadimos textos máis serios

--- src/data/text.php
+++ src/data/text.php
@@ -2,25 +2,38 @@
 
 return [
 
-	'lorem' => [
-		'author'    => 'Cicerón',
-		'title'     => 'Sobre los límites del bien y el mal',
-		'content'   => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
+	'o-pintureiro' => [
+		'author'    => 'Eugenio Carré Aldao',
+		'title'     => 'O Pintureiro',
+		'content'   => 'Erase que se era un fidalgo dos mais pobres, d\'aqueles que dixo Cervantes «de lanza en astillero, adarga antigua, rocin flaco y galgo corredor»     A facenda era pouca: as trampas moitas; mais, así e todo, tan pa- gado estaba dos seus pergaminos e executorias que inda non os trocara po-las minas do Potosí. Baixal-o lombo ao traballo non ó baixaba; se- gun él, por non luxal-a sua nobreza; segun as malas lingoas (quen as pode acamallar) por tere moito de folgazanería.     Vivía no lugarete na casa petrucial, que solo tiña de bô o dibuxado escudo riba da porta. O vento e a cluvia colabanse pol-as desvencilla- das ventás, onda os vidros non se vían facía moitos anos, e pol-as pou- cas tellas que cubrían a ripa do tellado, que era unha bendición. Os caneiros, pontonaxe e as táboas do piso, tan tramadas da traza es- taban que, para andar pol-o sobrado, tíñanse de facer mais equilibrios que para bailar na corda floxa. As paredes sostíñanse en pé sabe Dios como, pois solo a providencia podía termar d\'elas, nos días de treboa- da, pol-ó landreadas que estaban.     A roupa do fidalgo corría parexas c\'o bon estado da casa. Recor- dar, recordaba as modas do tempo do rei Perico; mais, en de por eso\' tan límpo do poo e palla andaba á forza do se acepillar, quo ao se pa- seare c\'o sombreiro â teima e c\'o seu pau de carballo na man, mália o lustroso do traxe, que de tan gastado deixaba vel-os fios da tea, pa- recía todo un cabaleiro.'
 	],
-	'dragonball1' => [
+
+	'campana-da-caprecorneca' => [
+		'author'    => 'Luís Otero Pimentel',
+		'title'     => 'Campaña da Caprecórneca',
+		'content'   => 'Cando un é mociño pásanlle cousas do trasno. Un día que fun á feira de Mezonzo, cando volvía prá miña casa, montado no meu faquiño, cerráronse de brétemas os montes de Lestedo, e colléronme as tenebras neles. Botouse a noite encima, negra como a concenza do usureiro, chovía moi miudiño, e eu non vía o camiño que levaba o faco, nin siquera as orellas deste. Xa perdera o tino, non sabía si iba ben ou mal, e deixei que a besta me levase pronde ela quixese. O cabalo tampouco debía estar moi seguro do camiño da nosa aldea, porque andaba perguiceiro e receloso como quen non sabe onde pon o pé: despois de moito tempo que anduvo de este xeito, non quixo andar máis e parouse. Eu xa levaba medo, pero aquela parada, púxome os pelos de punta, facéndome voar a monteira. Faleille polo baixo ó cabaliño, pra que seguise andando, pero el sigueu parado. Chamei por Palomo, o can que viña con nosoutros, pero non me respondeu.'
+	],
+
+	'os-probes-de-deus' => [
+		'author'    => 'Luís Amado Carballo',
+		'title'     => 'Os probes de Deus',
+		'content'   => 'Aos poucos, a chuvia foi amainando. Ficaba unha mañán gris, chea de friaxe, e o vento mareiro da Carnota, facía tremar as arbores molladas, con súpetos calofríos fugaces. Na longa sala dos dormitorios, ateigada de bafos cheirentos e de tépedo âr confinado, había somente catro vellos doentes, que de leito en leito parolaban mainiñamente, con voces cheas de langor e d\'esa resiñada melanconía que pon cadencias tenras, nas verbas indifrentes dos que agardan a morte sen espranza. Carón da fenestra, envolveito n-un vello cobertor rachado, outro home de ben comprida edá, ollaba o paisaxe da ría apreixando o narís contra os vidros, que o seu alento embazaba. N-aquela crua mañán de inverno, os mais asilados traballaban na horta, y-embaixo, na cociña, ouvíase o balbordo das risas e disputas das mulleres que arranxaban a louza e o xantar. O Pedro Lueiro, de cada vez atopábase mais falto de forzas, e unha perguiza invencibel, íbao gañando día a día, domando de cada instante con mais imperio â sua vôntade, que pol-a conta, asemade de dìa a día, era mais floxa. El, que aló na sua casiña de Valoa, levantábase ainda con estrelas para percorrer as leiras, legón ao lombo, sentíase agora apouvigado, vencido, desfeito por un mortal cansancio. A sua meirande ledicia era estar longas horas inmobel, coa vista vagariñante nas lexanías verdes e bretemosas d\'aquel paisaxe que tanto gostaba de contemplar dend\'as fenestras da casa de caridade.'
+	],
+
+	'maxina-ou-a-filla-espurea' => [
+		'author'    => 'Marcial Valladares Núñez',
+		'title'     => 'Maxina ou a filla espúrea',
+		'content'   => 'Era unha tarde á mediados d\'outono de mil oitocentos cincuenta; tarde apacible, que gusto e melancolía daba á un tempo: gusto, porque sentiño estaba todo, fijéras\'a vendima e, anque pouco e d\'uvas apestadas, fervía ja o mosto n\'as bodegas, maduro millo segaban os labradores n\'as suas veigas e, n\'os soutos, gardando-l-os rapaces-l-as castañas, apañaban os degaros qu\'aquí e acola caian d\'os ourizos; melancolía, porque case ningunha gala, ningun encanto restaba ja d\'a primavera e vrau; montes e campos sin froliñas; terras ond\'o centeo e trigo verdearan, árdigas, ou de color rajado; froiteiras, sin froita n\'os eijidos; bosques e carballeiras, inda verdeando, si, pro en silencio e amostrando tal cal rama niños de pajáros viajeiros qu\'alí cantaron; viñas co-a folla medio amarela e entr\'as vimieiras, ou n\'a enramada, anunciando o papo-rubio a vinda d\'o triste inverno. Era unha tarde, en fin, e tres señoras, Otilia, sua nai e Adria, donjela d\'esta, saindo á cousa d\'as duas e media de Santiago, marchaban por Sar abaijo dereito á Angrois, todas en bestas d\'arquiler e acompañadas de Leto, arquilador virollo, de poucas falas e que, mangas de camisa, iba braceando diante. Otilia, a mais nova d\'as señoras, rompia a marcha dempois de Leto. Nena de dezaseis anos, esbelta como unha abrótia, era de cara longuiña, blanco-sonrosada coma a margarida d\'as herbeiras; ollos negros e lucentes com\'acibeche; naris delgadiño, cal o pico d\'a lavandeira, ou cauda-tremula, e a sua graciosa boca, semellante o gomo d\'a viña qu\'empeza á abrir e amostra o tenro racimiño, amostraba tamen filas iguales d\'esmaltados dentes á través de bermellos beizos.'
+	],
+
+
+
+
+
+	'dragonball-intro' => [
 		'author'    => 'Dragon Ball',
-		'title'     => 'Chala Head Chala',
+		'title'     => 'Intro Dragon Ball',
 		'content'   => 'Atravesei unha nube brillante e o meu corpo tinguiuse de moitas cores distintas, aproveitei a explosión do volcán, e o planeta perdeu a súa vida dun golpe, seica un dinosauro atopo no meu camiño estou disposto a todo, a todos esmagarei, a todos. Nada, nada, estou disposto a todo, a calquera cousa que veña, nada nada, o río cruzarei moi veloz e ás peixes asustarei, nada nada, estou disposto a todo, a meterme na boca do lobo, aínda que me devore, nada nada, hoxe tamén manterei a miña cara sonrinte, a pesar de todo, a pesar de todo, a pesar de todo.'
-	],
-	'lorem2' => [
-		'author'    => 'Cicerón',
-		'title'     => 'Sobre los límites del bien y el mal',
-		'content'   => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
-	],
-	'lorem3' => [
-		'author'    => 'Cicerón',
-		'title'     => 'Sobre los límites del bien y el mal',
-		'content'   => 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed eiusmod tempor incidunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquid ex ea commodi consequat. Quis aute iure reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint obcaecat cupiditat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 	],
 
 ];
