d27407f657169ddd0d6583c47b1fb2ac16121bdd
Santiago Rodríguez <rocasan@gmail.com>
2020-07-20 20:55:16 +0200

    Added production artefacts routes in Klein (with mimetype corrections)

--- src/app.php
+++ src/app.php
@@ -8,6 +8,20 @@ $klein->respond(function (\Klein\Request $request, \Klein\Response $response, \K
 	$service->texts = require 'data/text.php';
 });
 
+$klein->respond('/src/assets/[script.min.js|style.css|style.css.map|galipsum.svg:filename]', function ($request, $response, $service) {
+    $mime = null;
+    if(preg_match('/(css|css.map)$/', $request->filename)) {
+        $mime = 'text/css';
+    }
+    else if(preg_match('/(js|js.map)$/', $request->filename)) {
+        $mime = 'text/javascript';
+    }
+    else if(preg_match('/(svg)$/', $request->filename)) {
+        $mime = 'image/svg+xml';
+    }
+    $response->file('./src/assets/' . $request->filename, $request->filename, $mime);
+});
+
 $klein->respond('GET', '/', function (\Klein\Request $request, \Klein\Response $response, \Klein\ServiceProvider $service, \Klein\App $app){
 	$service->title = 'GALipsum | Xerador de textos de recheo en galego';
 	$service->render(__DIR__ . '/templates/app.php');
